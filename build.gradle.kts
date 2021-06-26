5bdcf39ff781a1014fec0fe7771e867d8f52103e
Andreas Wassum <a.wassum@gmail.com>
2021-06-26 16:22:28 +0200

    Included dependencies for exposed and postegresql

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val hikari_version: String by project
+val postegresql_version: String by project
 
 plugins {
     application
@@ -26,6 +29,15 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation ("io.ktor:ktor-serialization:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Exposed ORM library
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+    implementation("org.postgresql:postgresql:$postegresql_version")
+
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
