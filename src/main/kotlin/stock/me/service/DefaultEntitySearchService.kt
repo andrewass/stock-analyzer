edd8785bd709adaed338e8a5fdce796405ab4d6e
Andreas Wassum <a.wassum@gmail.com>
2021-07-14 14:24:05 +0200

    Using regexp to query suggestions

--- src/main/kotlin/stock/me/service/DefaultEntitySearchService.kt
+++ src/main/kotlin/stock/me/service/DefaultEntitySearchService.kt
@@ -5,22 +5,36 @@ import kotlinx.serialization.json.Json
 import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.index.query.BoolQueryBuilder
 import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.index.query.RegexpFlag
 import org.elasticsearch.search.builder.SearchSourceBuilder
 import stock.me.model.Stock
 
 class DefaultEntitySearchService : EntitySearchService {
 
-    override fun getSuggestions(restClient: RestHighLevelClient, word: String): List<Stock> {
-        val request = SearchRequest()
+    override fun getSuggestions(restClient: RestHighLevelClient, query: String): List<Stock> {
+        val request = SearchRequest("stocks")
         val ssb = SearchSourceBuilder()
-        ssb.query(QueryBuilders.matchAllQuery())
+        ssb.query(createBoolQuery(query))
         request.source(ssb)
-
         val response = restClient.search(request, RequestOptions.DEFAULT)
 
         return response.hits.hits
             .map { it.sourceAsString }
             .map { Json.decodeFromString(it) }
     }
+
+    private fun createBoolQuery(query: String) =
+        BoolQueryBuilder()
+            .should(
+                QueryBuilders.regexpQuery("symbol", ".*$query.*")
+                    .caseInsensitive(true)
+                    .flags(RegexpFlag.ALL)
+            )
+            .should(
+                QueryBuilders.regexpQuery("description", ".*$query.*")
+                    .caseInsensitive(true)
+                    .flags(RegexpFlag.ALL)
+            )
 }
